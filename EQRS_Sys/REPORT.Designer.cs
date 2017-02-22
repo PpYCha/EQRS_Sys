a76a3433e6ec882d85adca27d041b0b0cd3e426d
PpYCha <diasandiaz7@gmail.com>
2017-02-23 06:40:44 +0800

    change ui

--- EQRS_Sys/REPORT.Designer.cs
+++ EQRS_Sys/REPORT.Designer.cs
@@ -80,7 +80,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(17, 113);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(413, 208);
+            this.dataGridView1.Size = new System.Drawing.Size(377, 208);
             this.dataGridView1.TabIndex = 4;
             // 
             // label2
